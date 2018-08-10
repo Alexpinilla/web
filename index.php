0f631943c4459d7427b738545f432623a5f0ec13
alexpinilla95 <alexpinilla95@gmail.com>
2018-08-10 19:10:21 +0200

    actualización web 100% por mí sin bootstrap ni ningún framework

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Alex Pinilla v2.0</title>
+    <meta name="author" content="Alex Pinilla" />
+    <meta description="description" content="Alex Pinilla, Programador y desarrollador web.">
+    <meta keywords="keywords" content="programador, diseñador web, desarrollador web">
+    <meta name="viewport" content="width=device-width, initial-scale=1" >
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/estilos.css" rel="stylesheet">
+    <link rel="shortcun icon" type="image/png" href="images/favicon.png">
+            <!-- JQUERY -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+            <!-- Script Opacidad -->
+    <script>
+    $(document).ready(function(){
+    $('#presentacion').fadeIn(2000);
+    });
+    </script>
+</head>
+<body>
+</body>
+</html>
